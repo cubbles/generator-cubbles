08ed7c1d56c34c4f5b0f60efd73fd8448ed98873
Judit Ross <jtrs@incowia.com>
2018-04-03 11:43:33 +0200

    generator-cubbles#1 update yeoman version, implemented choose of project type

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,29 +1,17 @@
 'use strict';
-var yeoman = require('yeoman-generator');
+var Generator = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 
-module.exports = yeoman.generators.Base.extend({
+module.exports = class extends Generator {
+  prompting () {
+    var done = this.async();
 
-    prompting: function() {
-        var done = this.async();
-
-        // Have Yeoman greet the user.
-        this.log(yosay(
-            'Welcome to the neat ' + chalk.red('Cubbles WebPackage-Project-Generator') +
-             '\nType' + '\n$ yo cubbles:project' + '\n to create a new cubbles-project.'
-        ));
-        done();
-    },
-
-    configuring: {},
-
-    writing: {},
-
-    install: function() {
-        //this.installDependencies();
-    },
-    end: function() {
-    }
-})
-;
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the neat ' + chalk.red('Cubbles WebPackage-Project-Generator') +
+      '\nType' + '\n$ yo cubbles:project' + '\n to create a new cubbles-project.'
+    ));
+    done();
+  }
+};
--- generators/project/index.js
+++ generators/project/index.js
@@ -1,97 +1,108 @@
 'use strict';
-var yeoman = require('yeoman-generator');
+var Generator = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
-var sh = require('shelljs');
+var path = require('path');
 
-module.exports = yeoman.generators.Base.extend({
+module.exports = class extends Generator {
+  constructor (args, opts) {
+    super(args, opts);
+    this.versions = [
+      'cubx.core.rte@3.x',
+      'cubx.core.rte@2.x'
+    ];
+  }
 
-    initializing: function() {
-    },
-    constructor: function() {
-        yeoman.generators.Base.apply(this, arguments);
-    },
+  prompting () {
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the neat ' + chalk.red('Cubbles WebPackage') +
+      ' generator!' +
+      '\n\n This will create a "cubbles" project into a new or existing sub-folder of your choice.'
+    ));
 
-    prompting: function() {
-        var done = this.async();
+    let prompts = [
+      {
+        type: 'input',
+        name: 'projectFolder',
+        message: 'Please provide a folder name: '
+      },
+      {
+        type: 'rawlist',
+        name: 'version',
+        message: 'Please decide for cubx.core.rte version: ',
+        choices: this.versions,
+        default: 0
+      }
+    ];
 
-        // Have Yeoman greet the user.
-        this.log(yosay(
-            'Welcome to the neat ' + chalk.red('Cubbles WebPackage') +
-            ' generator!' +
-            '\n\n This will create a "cubbles" project into a new or existing sub-folder of your choice.'
-        ));
+    return this.prompt(prompts).then((answers) => {
+      this.props = answers;
+    });
+  };
 
-        var prompts = [
-            {
-                type: 'input',
-                name: 'projectFolder',
-                message: 'Please provide a folder name: '
-            }
-        ];
-
-        this.prompt(prompts, function(props) {
-            this.props = props;
-            // To access props later use this.props.someOption;
-            done();
-        }.bind(this));
-    },
-
-    configuring: {},
-
-    writing: {
-        folder: function() {
-            sh.mkdir(this.props.projectFolder);
-            sh.cd(this.props.projectFolder);
-        },
-
-        webpackage: function() {
-            this.fs.copyTpl(
-                this.templatePath('%README.md'),
-                this.destinationPath('README.md'),
-                this.props
-            );
-            this.fs.copyTpl(
-              this.templatePath('%.gitignore'),
-              this.destinationPath('.gitignore'),
-              this.props
-            );
-
-            /** Now copy all the other stuff.
-             * @see https://github.com/isaacs/node-glob#options
-             */
-            var templatePath = this.templatePath();
-            this.fs.copy(
-                this.templatePath('./**/*'),
-                this.destinationRoot(), {
-                    globOptions: {
-                        dot: true,
-                        ignore: [templatePath + '/**/%*']
-                    }
-                }
-            );
-        }
-    },
-
-    install: function() {
-        this.log('\nInstalling DevTools dependencies ...');
-        this.spawnCommandSync('npm', ['install'], {cwd: 'devtools'});
-    },
-
-    end: function() {
-        this.log(yosay(
-            'Done'
-        ));
-        this.log(
-            'Note: Within the \'devtools\' folder, type \'grunt\' to get a list of the most important tasks.\n\n'
-        );
-        this.log(
-            'Now I\'m running grunt the first time:'
-        );
-        this.spawnCommandSync('grunt', [], {cwd: 'devtools'});
-
-        //PLAT-370: exit process here.
-        // Yeoman seems to have a bug processing the async-operation in 'prompting' correctly.
-        process.exit(0);
+  paths () {
+    let rootPath = path.join(process.cwd(), this.props.projectFolder);
+    this.destinationRoot(rootPath);
+    if (this.props[ 'version' ] === this.versions[ 1 ]) { //
+      // console.log('version', this.versions[ 1 ]);
+      let templatePath = path.join(__dirname, 'templates_2x');
+      this.sourceRoot(templatePath);
+      // console.log('sourceRoot', this.sourceRoot());
     }
-});
+  };
+
+  writing () {
+    this._webpackage();
+  };
+
+  install () {
+    this.log('\nInstalling DevTools dependencies ...');
+    this.spawnCommandSync('npm', [ 'install' ], { cwd: 'devtools' });
+  };
+
+  end () {
+    this.log(yosay(
+      'Done'
+    ));
+    this.log(
+      'Note: Within the \'devtools\' folder, type \'grunt\' to get a list of the most important tasks.\n\n'
+    );
+    this.log(
+      'Now I\'m running grunt the first time:'
+    );
+    this.spawnCommandSync('grunt', [], { cwd: 'devtools' });
+
+    // PLAT-370: exit process here.
+    // Yeoman seems to have a bug processing the async-operation in 'prompting' correctly.
+    process.exit(0);
+  };
+
+  _webpackage () {
+    // console.log('webpackage');
+    this.fs.copyTpl(
+      this.templatePath('%README.md'),
+      this.destinationPath('README.md'),
+      this.props
+    );
+    this.fs.copyTpl(
+      this.templatePath('%.gitignore'),
+      this.destinationPath('.gitignore'),
+      this.props
+    );
+
+    /** Now copy all the other stuff.
+     * @see https://github.com/isaacs/node-glob#options
+     */
+    var templatePath = this.templatePath();
+    console.log('templatePath', templatePath);
+    this.fs.copy(
+      [this.templatePath('./**/*'), '!%*', '!**/%*'],
+      this.destinationRoot(), {
+        globOptions: {
+          dot: true
+        }
+      }
+    );
+  };
+};
